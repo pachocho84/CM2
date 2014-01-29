a0e2804b5c00318bbe83057e55acb26a55064b52
EnoahNetzach <f.castellarin@gmail.com>
2014-01-29 18:32:15 +0100

    imagine filter thumbnail min

---
+++ src/CM/CMBundle/Service/ThumbnailMinFilterLoader.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace CM\CMBundle\Service;
+
+use Imagine\Filter\Basic\Thumbnail;
+use Imagine\Image\Box;
+use Imagine\Image\ImageInterface;
+use Liip\ImagineBundle\Imagine\Filter\Loader\LoaderInterface;
+
+class ThumbnailMinFilterLoader implements LoaderInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ImageInterface $image, array $options = array())
+    {
+        $mode = ImageInterface::THUMBNAIL_OUTBOUND;
+        if (!empty($options['mode']) && 'inset' === $options['mode']) {
+            $mode = ImageInterface::THUMBNAIL_INSET;
+        }
+
+        $size = $options['size'];
+
+        $imageSize = $image->getSize();
+        $origWidth = $imageSize->getWidth();
+        $origHeight = $imageSize->getHeight();
+
+        if ($origWidth / $origHeight < 1) {
+        	$width = $size;
+        	$height = (int)(($size / $origWidth) * $origHeight);
+        } else {
+        	$width = (int)(($size / $origHeight) * $origWidth);
+        	$height = $size;
+        }
+
+        if (($origWidth > $width || $origHeight > $height)
+            || (!empty($options['allow_upscale']) && ($origWidth !== $width || $origHeight !== $height))
+        ) {
+            $filter = new Thumbnail(new Box($width, $height), $mode);
+            $image = $filter->apply($image);
+        }
+
+        return $image;
+    }
+}
