393eb74d450aef446c2ea72c1fae06f16ce5bf0f
EnoahNetzach <f.castellarin@gmail.com>
2014-03-05 16:10:32 +0100

    minor fixes

--- src/CM/UserBundle/Controller/SecurityController.php
+++ src/CM/UserBundle/Controller/SecurityController.php
@@ -17,18 +17,20 @@ use FOS\UserBundle\Controller\SecurityController as BaseSecurityController;
 class SecurityController extends BaseSecurityController
 {
     private $template = 'FOSUserBundle:Security:login.html.twig';
+    private $templateArgs = array();
 
-    public function loginAction(Request $request, $template = null)
+    public function loginAction(Request $request, $template = null, $templateArgs = array())
     {
         if (!is_null($template)) {
             $this->template = $template;
         }
+        $this->templateArgs = $templateArgs;
 
         return parent::loginAction($request);
     }
 
     protected function renderLogin(array $data)
     {
-        return $this->container->get('templating')->renderResponse($this->template, $data);
+        return $this->container->get('templating')->renderResponse($this->template, array_merge($data, $this->templateArgs));
     }
 }
