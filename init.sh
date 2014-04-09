7f2532d02ccc490141a5ff33037ea55bde5d3836
EnoahNetzach <f.castellarin@gmail.com>
2014-04-09 17:26:29 +0200

    init.sh command

---
+++ init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+pushd $(dirname $0)
+
+env=${1:-prod}
+
+php app/console d:d:d --force -e=$env
+php app/console d:d:c -e=$env
+php app/console d:mi:m -n -e=$env
+php app/console d:f:l -n -e=$env
+
+popd
